0a2c62309f530244b121968882c3947322b42ad7
Paul Coupe <paul.coupe@digital.justice.gov.uk>
2021-11-17 15:06:18 +0000

    ARN-807 design content updates (#290)
    * ARN-807 design content updates
    
    * ARN-807 PR update

--- app/upw/controllers/start.js
+++ app/upw/controllers/start.js
@@ -3,7 +3,7 @@ const BaseController = require('../../common/controllers/baseController')
 class StartUnpaidWork extends BaseController {
   locals(req, res, next) {
     res.locals.pageDescription =
-      'Your answers will be combined with OASys and nDelius information. A full PDF version will then be created.'
+      'Your answers will be combined with OASys and nDelius information to create a PDF.<br>If you know nDelius and OASys information about the person needs changing, we advise you to do that before starting the assessment.'
 
     super.locals(req, res, next)
   }
