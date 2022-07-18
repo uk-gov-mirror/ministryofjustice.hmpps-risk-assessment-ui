3e0766d67224793e33f35997cde4ea3ca55b0fb3
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2022-07-18 12:11:39 +0100

    Arn 1046 accessibility statement page (#478)
    * Accessibility hyperlink in footer and web page created
    
    * Added content to accessibility page
    
    * adjusting heading sizes
    
    * adding spacing between sentence
    
    * Updated link on statement page

--- app/upw/steps.js
+++ app/upw/steps.js
@@ -375,4 +375,9 @@ module.exports = {
     noPost: true,
     template: `${__dirname}/templates/cookies.njk`,
   },
+  '/accessibility-statement': {
+    pageTitle: 'Accessibility statement for the Community Payback Assessment',
+    noPost: true,
+    template: `${__dirname}/templates/accessibility-statement.njk`,
+  },
 }
