1188092313bea56b65bfff13180de34f3acf9c34
Lucas Cairns <lucas.cairns@gmail.com>
2022-10-18 12:04:55 +0100

    Improve error handling in the start assessment controller (#569)

--- app/upw/controllers/start.js
+++ app/upw/controllers/start.js
@@ -34,52 +34,56 @@ class StartUnpaidWork extends BaseController {
 
   // eslint-disable-next-line consistent-return
   async saveValues(req, res, next) {
-    const { assessment } = req.session
-    const { eventId, assessmentCode, deliusEventType } = assessment
-    const { crn } = assessment.subject
+    try {
+      const { assessment } = req.session
+      const { eventId, assessmentCode, deliusEventType } = assessment
+      const { crn } = assessment.subject
 
-    logger.debug(`req.session.assessment: ${JSON.stringify(assessment)}`)
+      logger.debug(`req.session.assessment: ${JSON.stringify(assessment)}`)
 
-    const [assessmentCreated, createAssessmentResponse] = await createAssessment(
-      req.user,
-      crn,
-      eventId,
-      assessmentCode,
-      deliusEventType,
-    )
+      const [assessmentCreated, createAssessmentResponse] = await createAssessment(
+        req.user,
+        crn,
+        eventId,
+        assessmentCode,
+        deliusEventType,
+      )
 
-    logger.debug(`createAssessment response: ${JSON.stringify(createAssessmentResponse)}`)
+      logger.debug(`createAssessment response: ${JSON.stringify(createAssessmentResponse)}`)
 
-    if (!assessmentCreated) {
-      return res.render('app/error', { subHeading: getErrorMessageFor(req.user, createAssessmentResponse.reason) })
+      if (!assessmentCreated) {
+        return res.render('app/error', { subHeading: getErrorMessageFor(req.user, createAssessmentResponse.reason) })
+      }
+
+      const currentEpisode = await getCurrentEpisode(
+        createAssessmentResponse.assessmentUuid,
+        req.user?.token,
+        req.user?.id,
+      )
+
+      logger.debug(`getCurrentEpisode response: ${JSON.stringify(currentEpisode)}`)
+
+      // update subject details in session
+      assessment.subject = {
+        ...assessment.subject,
+        ...getSubjectDetailsFor(createAssessmentResponse),
+      }
+
+      assessment.uuid = createAssessmentResponse?.assessmentUuid
+      assessment.episodeUuid = currentEpisode?.episodeUuid
+      assessment.lastEditedBy = currentEpisode?.userFullName
+      assessment.lastEditedDate = currentEpisode?.lastEditedDate
+
+      req.session.assessment = assessment
+
+      logger.debug(`saving assessment to session: ${JSON.stringify(assessment)}`)
+
+      req.session.save()
+
+      super.saveValues(req, res, next)
+    } catch (e) {
+      next(new Error('Failed to create assessment'))
     }
-
-    const currentEpisode = await getCurrentEpisode(
-      createAssessmentResponse.assessmentUuid,
-      req.user?.token,
-      req.user?.id,
-    )
-
-    logger.debug(`getCurrentEpisode response: ${JSON.stringify(currentEpisode)}`)
-
-    // update subject details in session
-    assessment.subject = {
-      ...assessment.subject,
-      ...getSubjectDetailsFor(createAssessmentResponse),
-    }
-
-    assessment.uuid = createAssessmentResponse?.assessmentUuid
-    assessment.episodeUuid = currentEpisode?.episodeUuid
-    assessment.lastEditedBy = currentEpisode?.userFullName
-    assessment.lastEditedDate = currentEpisode?.lastEditedDate
-
-    req.session.assessment = assessment
-
-    logger.debug(`saving assessment to session: ${JSON.stringify(assessment)}`)
-
-    req.session.save()
-
-    super.saveValues(req, res, next)
   }
 }
 
